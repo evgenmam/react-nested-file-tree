5c515a20a42cbe9112d82f1de62f7d712b2d8b87
Evgen <evgenomam@gmail.com>
2022-06-19 22:28:48 +0300

    Branch by redux

--- src/TreePage/Container/Trunk/LabelChildren/Label/Label.tsx
+++ src/TreePage/Container/Trunk/LabelChildren/Label/Label.tsx
@@ -3,8 +3,8 @@ import { useSelector } from 'react-redux';
 import { Item } from '../../../../../api/data';
 import { LabelChildren } from '../LabelChildren';
 import { selectLevel } from '../../../../../store/data/selectors';
-// import { useDispatch } from 'react-redux';
-// import { actions } from 'store/data/data';
+import { useDispatch } from 'react-redux';
+import { actions } from 'store/data/data';
 
 import styles from '../../../../../styles/Home.module.css';
 
@@ -12,22 +12,17 @@ interface Props {
   item: Item;
 }
 export const Label: React.FC<Props> = ({ item }) => {
+  const dispatch = useDispatch();
   const selectedLevel = useSelector(selectLevel)
   const [style, setStyle] = useState(styles.rootlevel);
   useEffect(() => {
     if (selectedLevel > 0) {
       setStyle('line');
     }
-  },[setStyle, selectedLevel]);
-
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   if (item.parentId === -1){
-  //     dispatch(actions.resetLevel());
-  //   } 
-  // },[]);
-
+    if (item.parentId === -1){
+      dispatch(actions.resetLevel());
+    } 
+  },[setStyle, selectedLevel, item.parentId, dispatch]);
 
   return (
     <div>
--- src/TreePage/Container/Trunk/LabelChildren/LabelChildren.tsx
+++ src/TreePage/Container/Trunk/LabelChildren/LabelChildren.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Item } from 'api/data';
 import { Label } from './Label/Label';
-// import { useDispatch } from 'react-redux';
-// import { actions } from 'store/data/data';
+import { useDispatch } from 'react-redux';
+import { actions } from 'store/data/data';
 
 interface Props {
   items: Item[];
 }
 
 export const LabelChildren: React.FC<Props> = ({ items }) => {
-  // const dispatch = useDispatch();
-  // dispatch(actions.increaseLevel());
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (items.length !== 0){
+      dispatch(actions.increaseLevel());
+    } 
+  },[dispatch, items.length]);
+
 
   return (
     <div>
